4a5920595c0d59ce6fb3a4ca66cc8723621ee28c
Harikrishna Gunnala <89335989+itsmehk8333@users.noreply.github.com>
2025-09-16 12:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+// your JS code here. If required.
+
+const counterButton = document.querySelector("#incrementBtn");
+const counterValue = document.querySelector("#counter");
+
+counterButton.addEventListener("click", () => {
+    let currentValue = parseInt(counterValue.innerText);
+    alert(currentValue); // show un-incremented value
+    counterValue.innerText = currentValue + 1; // update UI
+});
